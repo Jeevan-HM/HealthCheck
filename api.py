9ee48fc18208d23160b9a8fe3086ad2fa75c24e3
Jeevan-HM <jeevanhm308@gmail.com>
2020-10-29 00:38:33 +0530

    Create XML generator from Database

---
+++ api.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+import config as cfg
+import xml.etree.ElementTree as e
+
+client = MongoClient(cfg.uri)
+db_name = cfg.dbname
+db = client[db_name]
+data = cfg.data
+
+# def insert_data(data):
+#     document = db.collection.insert_one(data)
+# insert_data(data)
+person = [data.get("person1_id")]
+person[0]["api_url"]
+version = "1.0"
+txn = person[0]["txn"]
+sample_data = [person[0]["sample _data"]]
+print(sample_data)
+
+root = e.Element("PullURIRequest ver=" + version + " txn" + txn)
+doc_details = e.SubElement(root, "DocDetails")
+for i in sample_data:
+    e.SubElement(doc_details, "UID").text = i["UID"]
+    e.SubElement(doc_details, "DOB").text = i["DOB"]
+    e.SubElement(doc_details, "Full_Name").text = i["Full_Name"]
+    e.SubElement(doc_details, "gender").text = i["gender"]
+    e.SubElement(doc_details, "phone_number").text = i["phone_number"]
+
+e.SubElement(root, person[0]["email"])
+xml = e.ElementTree(root)
+xml.write("TEST.xml")
+print(xml)
---
+++ config.py
@@ -0,0 +1,19 @@
+uri = "mongodb://localhost:27017"
+dbname = "Sample_data"
+collection = "SampleInput"
+
+data = {
+    "person1_id": {
+        "api_url": "https://register/api/format/xml",
+        "sample _data": {
+            "UID": "1234 5678 9123",
+            "DOB": "01/01/2001",
+            "Full_Name": "Jeevan HM",
+            "gender": "male",
+            "phone_number": "1234567890",
+        },
+        "email": "test@gmail.com",
+        "txn": "1a2b3cd4",
+        "status": 1,
+    }
+}
