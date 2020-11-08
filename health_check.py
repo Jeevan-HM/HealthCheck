4e7cbb36c7b8f4f557eefaa3a51ddad26ecb672b
Jeevan HM <jeevanhm308@gmail.com>
2020-11-09 00:25:44 +0530

    Tested response of api

--- health_check.py
+++ health_check.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, Response
 from flask_restful import Api, Resource, reqparse
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from xml.dom import minidom
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,41 +17,39 @@ db = client[db_name]
 data = cfg.data
 
 
-class name_matching(Resource):
+class xml_generator(Resource):
     def get(self):
 
-        try:
-            global data
-            # db.collection.insert_one(data)
-            data = data.get("sample_data")
-            root = et.Element(
-                "PullURIRequest",
-                attrib={
-                    "xmlns": "http://tempuri.org/",
-                    "ver": "1.0",
-                    "ts": str(ObjectId("5c51aca67c76124020edbbaf").generation_time),
-                    "txn": data["txn"],
-                    "orgId": data["orgId"],
-                    "format": "xml/pdf/both",
-                },
-            )
-            document_details = data["doc_details"]
-            doc_details = et.SubElement(root, "DocDetails")
+        global data
+        # db.collection.insert_one(data)
+        data = data.get("sample_data")
+        root = et.Element(
+            "PullURIRequest",
+            attrib={
+                "xmlns": "http://tempuri.org/",
+                "ver": "1.0",
+                "ts": str(ObjectId("5c51aca67c76124020edbbaf").generation_time),
+                "txn": data["txn"],
+                "orgId": data["orgId"],
+                "format": "xml/pdf/both",
+            },
+        )
+        document_details = data["doc_details"]
+        doc_details = et.SubElement(root, "DocDetails")
 
-            for key in document_details:
-                et.SubElement(doc_details, key).text = document_details[key]
-            xml = et.ElementTree(root)
-            xml.write("json_to_xml.xml")
-            xml = """my xml"""
-            headers = {"Content-Type": "application/xml"}
-            response = requests.post(
-                "http://127.0.0.1:5000/test", data=xml, headers=headers
-            )
-            print(response.elapsed)
-        except:
-            return "API server not functioning"
+        for key in document_details:
+            et.SubElement(doc_details, key).text = document_details[key]
+        xml = et.ElementTree(root)
+        xml.write("json_to_xml.xml")
+        xml = """json_to_xml.xml"""
+        headers = {"Content-Type": "application/xml"}
+        URL = "http://0.0.0.0:5000/test"
+        response = requests.get(URL, data=xml, headers=headers)
+        json_response = response.json()
+        latency = response.elapsed
+        return jsonify({"Response": json_response})
 
 
-api.add_resource(name_matching, "/")
+api.add_resource(xml_generator, "/")
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
